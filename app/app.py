ba719e66a51b7048541457ee9ffa46d3296c82a1
CatSatOK <68549428+CatSatOK@users.noreply.github.com>
2023-12-08 09:23:17 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -118,5 +118,5 @@ def cancel():
 #launching
 if __name__ == "__main__":
   #only run directly like this if not production
-  app.run(host="0.0.0.0", port=8080) 
+  app.run(host="0.0.0.0", port=5000) 
   
